326018be59507e9eb8b260642075e9275d737f99
Mvsp83 <marcusviniciussp.dev@gmail.com>
2024-11-28 22:18:43 -0300

    Ajustes na tela de Login - Dados sobre Aplicativo

--- InstitutoTriboDeDavi.FLUTTER/flutter_tribo_de_davi_api/lib/views/login.dart
+++ InstitutoTriboDeDavi.FLUTTER/flutter_tribo_de_davi_api/lib/views/login.dart
@@ -47,112 +47,190 @@ class LoginPageState extends State<LoginPage> {
     }
   }
 
+  void _showAboutDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: AppTheme.primaryColor,
+          title: Text(
+            textAlign: TextAlign.center,
+            "Sobre o Aplicativo",
+            style: AppTheme.bodyTextStyle.copyWith(
+              color: AppTheme.textColor,
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                "Desenvolvido por:\n",
+                style: AppTheme.bodyTextStyle.copyWith(
+                  color: AppTheme.textColor,
+                ),
+              ), // Imagem
+              Image.asset(
+                'lib/assets/images/emeve83-2.png',
+                height: 100,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Versão:\n v.1.0",
+                style: AppTheme.bodyTextStyle.copyWith(
+                  color: AppTheme.textColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                textAlign: TextAlign.center,
+                "Ferramentas Utilizadas:\n - Flutter\n - API em C#",
+                style: AppTheme.bodyTextStyle.copyWith(
+                  color: AppTheme.textColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Contato:",
+                style: AppTheme.bodyTextStyle.copyWith(
+                  color: AppTheme.textColor,
+                ),
+              ),
+              Text(
+                "contato@empresa.com",
+                style:
+                    AppTheme.bodyTextStyle.copyWith(color: AppTheme.textColor),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "Fechar",
+                style: AppTheme.bodyTextStyle.copyWith(
+                  color: AppTheme.textColor,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.primaryColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 80.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Image.asset(
-                  'lib/assets/images/logo.png',
-                  height: 150,
-                ),
-              ),
-              const SizedBox(height: 10),
-              // Campo de Email
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  prefixIcon: const Icon(Icons.email),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
+        backgroundColor: AppTheme.primaryColor,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 80.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'lib/assets/images/logo.png',
+                    height: 150,
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 20),
-              // Campo de Senha
-              TextField(
-                controller: _passwordController,
-                obscureText: !_isPasswordVisible,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  prefixIcon: const Icon(Icons.lock),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    prefixIcon: const Icon(Icons.email),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isPasswordVisible = !_isPasswordVisible;
-                      });
-                    },
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 20),
                   ),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  keyboardType: TextInputType.emailAddress,
                 ),
-              ),
-              const SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerRight,
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 20),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Forgot Password?',
+                    style: AppTheme.bodyTextStyle.copyWith(
+                      color: AppTheme.textColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _login,
+                    style: AppTheme.elevatedButtonStyle,
+                    child: const Text(
+                      'Login',
+                      style: AppTheme.buttonTextStyle,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: AppTheme.primaryColor,
+          child: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              GestureDetector(
+                onTap: _showAboutDialog,
                 child: Text(
-                  'Forgot Password?',
+                  '© ${DateTime.now().year} - eMeVe83 Tech',
                   style: AppTheme.bodyTextStyle.copyWith(
                     color: AppTheme.textColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
                   ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-              const SizedBox(height: 40),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: _login,
-                  style: AppTheme.elevatedButtonStyle,
-                  child: const Text(
-                    'Login',
-                    style: AppTheme.buttonTextStyle,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-            ],
+            ]),
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: AppTheme.primaryColor,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text(
-            '© ${DateTime.now().year} - EmEvE83 - v.1.0',
-            style: AppTheme.bodyTextStyle.copyWith(
-              color: AppTheme.textColor,
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
